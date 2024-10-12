b41b06a52b87e8e57e9ba4d9fe36bee17eb52933
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-10-12 11:40:04 -0300

    first commit

---
+++ src/main/java/com/danielproject/spring_boot_estudo/SpringBootEstudoApplication.java
@@ -0,0 +1,13 @@
+package com.danielproject.spring_boot_estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEstudoApplication.class, args);
+	}
+
+}

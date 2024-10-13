01ad07c3425b0fc1dfbde608085fa221f12dccf4
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-10-13 09:41:36 -0300

    making first tests

--- src/main/java/com/danielproject/spring_boot_estudo/SpringBootEstudoApplication.java
+++ src/main/java/com/danielproject/spring_boot_estudo/SpringBootEstudoApplication.java
@@ -2,12 +2,20 @@ package com.danielproject.spring_boot_estudo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootEstudoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEstudoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index(){
+		return "Hello";
 	}
 
 }

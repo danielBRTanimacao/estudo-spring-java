29cab9215e9ca5f50efc9bda2dca3a132c28758f
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-10-18 13:49:39 -0300

    more tests using method POST

--- src/main/java/com/daniel/spring_boot_estudo/configuration/HelloConfiguration.java
+++ src/main/java/com/daniel/spring_boot_estudo/configuration/HelloConfiguration.java
@@ -1,15 +1,15 @@
 package com.daniel.spring_boot_estudo.configuration;
 
-import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HelloConfiguration {
 
-    @Bean
-    public MyService myService(){
-        return new MyServiceImpl();
-    }
+    // @Bean
+    // public MyService myService(){
+    //     return new MyServiceImpl();
+    // }
     // @Bean
     // public SDKAWS sdkAWS(){
     //     return new SDKAWS();
--- src/main/java/com/daniel/spring_boot_estudo/controller/HelloWorldController.java
+++ src/main/java/com/daniel/spring_boot_estudo/controller/HelloWorldController.java
@@ -1,12 +1,18 @@
 package com.daniel.spring_boot_estudo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.daniel.spring_boot_estudo.domain.User;
 import com.daniel.spring_boot_estudo.service.HelloWorldService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -15,11 +21,17 @@ public class HelloWorldController {
     @Autowired
     private HelloWorldService helloWorldService;
 
-    @Autowired
-    private SDKAWS sdkAWS;
+    // @Autowired
+    // private SDKAWS sdkAWS;
     
     @GetMapping
-    public String HelloWorld(){
+    public String helloWorld(){
         return helloWorldService.Test("Daniel");
     }
+
+    @PostMapping("/{id}")
+    public String helloWorldPost(@PathVariable String id,@RequestParam(value = "filter", defaultValue = "none") String filter , @RequestBody User body) {
+        return id + " Ola POST " + body.getName() + " " + body.getEmail();
+    }
+    
 }

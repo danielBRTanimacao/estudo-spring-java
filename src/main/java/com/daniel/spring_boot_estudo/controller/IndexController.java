bfd410945ae60cbe2f9caff50f0e6e80fd30f65f
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-10-23 12:15:51 -0300

    init tests on spring

---
+++ src/main/java/com/daniel/spring_boot_estudo/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.daniel.spring_boot_estudo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+}

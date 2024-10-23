ed85f859b7449199df4fb1681c84ab9a7a97eb3a
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-10-23 17:33:28 -0300

    trying to add render html

--- src/main/java/com/daniel/spring_boot_estudo/SpringBootEstudoApplication.java
+++ src/main/java/com/daniel/spring_boot_estudo/SpringBootEstudoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootEstudoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEstudoApplication.class, args);
 	}
-
 }
--- src/main/java/com/daniel/spring_boot_estudo/controller/IndexController.java
+++ src/main/java/com/daniel/spring_boot_estudo/controller/IndexController.java
@@ -1,12 +1,14 @@
 package com.daniel.spring_boot_estudo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
-import org.springframework.web.bind.annotation.RestController;
-
-
-@RestController
-@RequestMapping("/")
+@Controller
 public class IndexController {
-
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title", "21 game");
+        return "index";
+    }
 }
